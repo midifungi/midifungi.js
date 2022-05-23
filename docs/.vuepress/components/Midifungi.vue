42c1d125c6a173a5a8c226c02016da6eb810f508
Midifungi <midifungi@gmail.com>
2022-05-23 13:23:34 -0700

    Attempts to fix dynamic import for build

--- docs/.vuepress/components/Midifungi.vue
+++ docs/.vuepress/components/Midifungi.vue
@@ -22,11 +22,35 @@ export default {
       if (path[0] === '@') {
         path = path.substr(1)
       }
-      import(`../public/sketch/${path}.js`).then(m => {
-        Layers.target = this.$refs.target
-        m.default()
-      })
+      this.loadScript(path)
     })
+  },
+
+  methods: {
+    async loadScript (path) {
+      // Unfortunately we have to split this out with vite
+      // @see https://github.com/vitejs/vite/issues/4945#issuecomment-951770052
+      const splitName = path.split('/')
+      let sketch
+      if (splitName.length === 1) {
+        sketch = await import(`../public/sketch/${splitName[0]}.js`)
+      } else if (splitName.length === 2) {
+        sketch = await import(`../public/sketch/${splitName[0]}/${splitName[1]}.js`)
+      } else if (splitName.length === 3) {
+        sketch = await import(`../public/sketch/${splitName[0]}/${splitName[1]}/${splitName[2]}.js`)
+      } else if (splitName.length === 4) {
+        sketch = await import(`../public/sketch/${splitName[0]}/${splitName[1]}/${splitName[2]}/${splitName[3]}.js`)
+      } else if (splitName.length === 5) {
+        sketch = await import(`../public/sketch/${splitName[0]}/${splitName[1]}/${splitName[2]}/${splitName[3]}/${splitName[4]}.js`)
+      } else if (splitName.length === 6) {
+        sketch = await import(`../public/sketch/${splitName[0]}/${splitName[1]}/${splitName[2]}/${splitName[3]}/${splitName[4]}/${splitName[5]}.js`)
+      } else if (splitName.length === 7) {
+        sketch = await import(`../public/sketch/${splitName[0]}/${splitName[1]}/${splitName[2]}/${splitName[3]}/${splitName[4]}/${splitName[5]}/${splitName[6]}.js`)
+      }
+
+      Layers.target = this.$refs.target
+      sketch.default()
+    }
   }
 }
 </script>
