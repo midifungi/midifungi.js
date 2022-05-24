1f9e355b41d765ba77a4935882bbdc7ffc55d4bb
Midifungi <midifungi@gmail.com>
2022-05-23 22:28:01 -0700

    constole.log module to see on deploy

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -3,7 +3,8 @@ import {defineClientConfig} from '@vuepress/client'
 export default defineClientConfig({
   async enhance () {
     if (!__VUEPRESS_SSR__) {
-      await import ('../../node_modules/p5/lib/p5.min.js')
+      const p = await import ('../../node_modules/p5/lib/p5.min.js')
+      console.log(p)
       await import('../../src/midifungi.js')
       new window.p5()
     }
