8449638c330636e3c5ab921cd7231ded7a19175c
Midifungi <midifungi@gmail.com>
2022-06-14 16:28:03 -0700

    Fill border with colors

--- docs/.vuepress/public/sketch/220614/sketch.js
+++ docs/.vuepress/public/sketch/220614/sketch.js
@@ -5,6 +5,7 @@ class Square {
   }
 
   draw () {
+    fill(this.color)
     rect(this.x, this.y, this.size, this.size)
   }
 }
@@ -13,23 +14,39 @@ Layers.create(() => {
   new Layer({
     id: 'border',
 
-    menu: {},
+    menu: {
+      bg () {return this.colors}
+    },
     store: {
       squares: []
     },
 
     setup () {
-      let horizCount = ~~random(10, 20)
-      let size = width/horizCount
+      // Find a size that fills the border with squares perfectly
+      // @see https://stackoverflow.com/questions/62015484/grid-of-squares-that-fits-within-any-width-height-container
+      let minSquareSize = maxSize *.05
+      let maxSquareSize = maxSize * .1
+      let ratio = width/height
+      if (ratio > 1) {
+        ratio = 1/ratio
+      }
+      let size = maxSquareSize*ratio
+      if (size < minSquareSize) {
+        size = minSquareSize
+      }
+      
+      let horizCount = width/size
       let vertCount = height/size
       
+      let colorCount = 0
       $squares = []
       // North
       for (let i = 0; i < horizCount; i++) {
         $squares.push(new Square({
           size,
           x: i*size,
-          y: 0
+          y: 0,
+          color: this.colors[wrap(colorCount++, 0, this.colors.length)]
         }))
       }
       // East
@@ -37,15 +54,17 @@ Layers.create(() => {
         $squares.push(new Square({
           size,
           x: width-size,
-          y: i*size
+          y: i*size,
+          color: this.colors[wrap(colorCount++, 0, this.colors.length)]
         }))
       }
       // South
-      for (let i = horizCount; i > -1; i--) {
+      for (let i = horizCount; i > 0; i--) {
         $squares.push(new Square({
           size,
           x: i*size,
-          y: height-size
+          y: height-size,
+          color: this.colors[wrap(colorCount++, 0, this.colors.length)]
         }))
       }
       // West
@@ -53,12 +72,14 @@ Layers.create(() => {
         $squares.push(new Square({
           size,
           x: 0,
-          y: i*size
+          y: i*size,
+          color: this.colors[wrap(colorCount++, 0, this.colors.length)]
         }))
       }
     },
 
     draw () {
+      background($bg)
       $squares.forEach(s => s.draw())
     }
   })
