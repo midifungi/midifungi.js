ecaf035064bd1eebbcc1e1e17d6baec97078e36c
Midifungi <midifungi@gmail.com>
2022-07-15 20:23:57 -0700

    #42 Adds imagination and more family

--- docs/.vuepress/public/sketch/3/emoji.js
+++ docs/.vuepress/public/sketch/3/emoji.js
@@ -6,17 +6,25 @@ export default function () {
     ['👩🏾','👨🏾','🧑🏾','👵🏾','👴🏾','🧓🏾','👩🏾‍🦰','👨🏾‍🦰','🧑🏾‍🦰','👩🏾‍🦱','👨🏾‍🦱','🧑🏾‍🦱','👩🏾‍🦲','👨🏾‍🦲','🧑🏾‍🦲','👩🏾‍🦳','👨🏾‍🦳','🧑🏾‍🦳','👱🏾‍♀️','👱🏾‍♂️','👱🏾','👳🏾‍♀️','👳🏾‍♂️','👳🏾','🧔🏾','🧔🏾‍♂️','🧔🏾‍♀️'],
     ['👩🏿','👨🏿','🧑🏿','👵🏿','👴🏿','🧓🏿','👩🏿‍🦰','👨🏿‍🦰','🧑🏿‍🦰','👩🏿‍🦱','👨🏿‍🦱','🧑🏿‍🦱','👩🏿‍🦲','👨🏿‍🦲','🧑🏿‍🦲','👩🏿‍🦳','👨🏿‍🦳','🧑🏿‍🦳','👱🏿‍♀️','👱🏿‍♂️','👱🏿','👳🏿‍♀️','👳🏿‍♂️','👳🏿','🧔🏿','🧔🏿‍♂️','🧔🏿‍♀️']   
   ]
-  const children = [
+  const siblings = [
     ['👧🏻','👦🏻','🧒🏻'],
     ['👧🏼','👦🏼','🧒🏼'],
     ['👧🏽','👦🏽','🧒🏽'],
     ['👧🏾','👦🏾','🧒🏾'],
     ['👧🏿','👦🏿','🧒🏿']
   ]
+  const self = [
+    ['🧑🏻‍💻','👨🏻‍💻','👩🏻‍💻'],
+    ['🧑🏼‍💻','👨🏼‍💻','👩🏼‍💻'],
+    ['🧑🏽‍💻','👨🏽‍💻','👩🏽‍💻'],
+    ['🧑🏾‍💻','👨🏾‍💻','👩🏾‍💻'],
+    ['🧑🏿‍💻','👨🏿‍💻','👩🏿‍💻']
+  ]
+  const imagination = ['✨','🎶','🎵','⭐','🌟','❤️','🧡','💛','💚','💙','💜','👻','👽','👾','🐵','🐶','🐺','🐱','🦁','🐯','🦊','🦝','🐮','🐷','🐭','🐹','🐰','🐻','🐻‍❄️','🐨','🐼','🐸','🦖','🦕','🐉','🐍','🐢','🐙','🐧','🐥','🐣','🦋','🐝','🐞','🪲']
 
   Layers.create(() => {
     const size = minSize*.3
-    let numGuardians = ~~random(1, 6)
+    let numGuardians = ~~random(50, 100)
 
     // if (random() < .7) {
     //   numGuardians = 2
@@ -40,51 +48,75 @@ export default function () {
       
       store: {
         family: [],
-        self: {}
+        selfIdx: 0,
+        bubble: {},
+        imaginationCount: random(500, 1000),
+        imagination: [],
+        imaginationCanvas: null
+      },
+
+      onDispose () {
+        $imaginationCanvas && $imaginationCanvas.dispose()
       },
 
       setup () {
         // Reset things
         $family = []
+        $bubble = {
+          shape: 'box',//random(['box', 'sphere']),
+          detailX: ~~random(3, 6),
+          detailY: ~~random(3, 9),
+          rotX: random(PI*2),
+          rotY: random(PI*2),
+          rotZ: random(PI*2),
+        }
         
         // Canvas
+        $imaginationCanvas = createGraphics(width, height)
+        $imaginationCanvas.textAlign(CENTER, CENTER)
         offscreen.textAlign(CENTER, CENTER)
         canvas.setAttributes('alpha', true)
-        canvas.drawingContext.disable(canvas.drawingContext.DEPTH_TEST)
 
         // Create family and create smear points for each face
         for (let i = 0; i < $numFamily+1; i++) {
           let emoji
           let tone = ~~random(5)
           let emojiHeight
-          let isChild = false
+          let isSelf = false
+          let x
           let z
+          let rot
 
           // Different settings for guardians and children
           if (i < $numFamily) {
             emoji = random(busts[tone])
-            emojiHeight = random(size*.75, size*1.25)
+            emojiHeight = random(-size*.25, size*1.25)
+            x = random(size/2, width-size/2)
             z = random(-size*2, -size*.5)
+            rot = random(-PI/6, PI/6)
           } else {
-            emoji = random(children[tone])
-            emojiHeight = random(height/2+size*.15, height/2+size*1.25)
-            isChild = true
-            z = random(-size*.25, size*.25)
+            emoji = random(self[tone])
+            emojiHeight = random(height/2+size*.85, height/2+size*1.2)
+            isSelf = true
+            x = width/2//random(size*1.25, width-size*1.25)
+            z = random(0, size*.25)
+            rot = random(-PI/12, PI/12)
           }
           
           // Create family member
           const fam = {
             emoji,
-            x: random(size, width-size),
+            x,
             height: emojiHeight,
             z,
             tone,
-            isChild
+            isSelf,
+            rot
           }
           $family.push(fam)
 
           // Pick a bunch of random points to smear
-          if (!isChild) {
+          if (!isSelf) {
             const smear = []
             for (let j = 0; j < 1000; j++) {
               const x = random(fam.x-size*2, fam.x+size*2)
@@ -106,10 +138,29 @@ export default function () {
         }
 
         // Sort family by z for transparent buffer drawing
-        $family.sort((a, b) => b.z - a.z)
+        $family.sort((a, b) => b.height - a.height)
+        // Get the index of self
+        $selfIdx = $family.findIndex(fam => fam.isSelf)
+
+        // Create imagination
+        $imagination = []
+        for (let i = 0; i < $imaginationCount; i++) {
+          const angle = random(PI*2)
+          const len = random(size)
+          
+          $imagination.push({
+            emoji: random(imagination),
+            x: sin(angle) * len,
+            y: cos(angle) * len,
+            z: random(-size*1.5, -size),
+            rot: random(-PI/6, PI/6),
+            size: random(size*.25, size*.5)
+          })
+        }
       },
 
       draw () {
+        drawingContext.enable(drawingContext.DEPTH_TEST)
         clear()
         background(0,0)
         
@@ -119,15 +170,16 @@ export default function () {
           offscreen.clear()
 
           // Set size
-          if (fam.isChild) {
+          if (fam.isSelf) {
             offscreen.textSize(size*.75)
+            offscreen.text(fam.emoji, width/2, height/2)
           } else {
             offscreen.textSize(size)
+            offscreen.text(fam.emoji, fam.x, height/2)
           }
-          offscreen.text(fam.emoji, fam.x, height/2)
 
           // Get the points
-          if (!fam.isChild) {
+          if (!fam.isSelf) {
             fam.smear.forEach((point, n) => {
               // @todo use pixels bc faster
               point.color = offscreen.get(point.x, point.y)
@@ -160,11 +212,61 @@ export default function () {
             offscreen.colorMode(...this.colorMode)
           }
   
+          // Set depth test (for transparent plane rendering)
+          if (fam.isSelf) {
+            drawingContext.enable(drawingContext.DEPTH_TEST)
+          } else {
+            drawingContext.disable(drawingContext.DEPTH_TEST)
+          }
+
+          // Draw imagination
+          if (fam.isSelf) {
+            noStroke()
+            drawingContext.disable(drawingContext.DEPTH_TEST)
+            // drawingContext.globalCompositionOperation = 'destination-over'
+            for (let j = 0; j < $imaginationCount; j++) {
+              const img = $imagination[j]
+              $imaginationCanvas.clear()
+              $imaginationCanvas.textSize(size)
+              $imaginationCanvas.text(img.emoji, width/2, height/2)
+              texture($imaginationCanvas)
+              push()
+              translate(fam.x-width/2, fam.height-height/2, fam.z)
+              translate(img.x, img.y+size/2, img.z)
+              rotateY(img.rot)
+              plane(img.size, img.size)
+              pop()
+            }
+            // drawingContext.globalCompositionOperation = 'source-over'
+          }
+
+          // Draw bubble
+          if (fam.isSelf) {
+            push()
+            translate(fam.x-width/2, fam.height-height/2, fam.z)
+
+            stroke(255)
+            strokeWeight(max(1, minSize*.0025))
+            noFill()
+            switch ($bubble.shape) {
+              case 'box':
+                rotateY($bubble.rotY)
+                box(size*1.25)
+              break
+              case 'sphere':
+                rotateY($bubble.rotY)
+                sphere(size, $bubble.detailX, $bubble.detailY)
+              break
+            }
+            pop()
+          }
+
+          // Draw emoji
           texture(offscreen)
           noStroke()
           push()
           translate(fam.x-width/2, fam.height-height/2, fam.z)
-          rotateY(random(-PI/6, PI/6))
+          rotateY(fam.rot)
           plane(width, height)
           pop()
         }
